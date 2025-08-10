961aad38ee18d0b6ec4ffff33b4431f81a420b94
Naman Lazarus <namanlazarus02@gmail.com>
2025-08-10 20:51:10 +0530

    Added DeepSignTrainer:train method

--- scripts/training.py
+++ scripts/training.py
@@ -2,6 +2,7 @@
 
 import logging
 import tensorflow as tf
+from huggingface_hub import hf_hub_download
 
 from utils import load_config
 from preprocess import DataParser
@@ -35,53 +36,86 @@ class DeepSignTrainer:
         else:
             logging.info(f"Dataset loaded.")
 
-        # Build and get the model
+        # Get the compiled model
         self.model = self._get_model()
 
         
-    def _get_dataset(self) -> tf.data.Dataset:
+    def _get_dataset(self, source: str = 'local') -> tf.data.Dataset:
         """        
         Loads the training dataset from the specified TFRecord file.
 
         Returns:
             tf.data.Dataset: A TensorFlow dataset containing the training data.
         """
+        if source == 'HF_hub':
+            # Download data from Hugging Face Hub
+            data_path = self._download_data_from_HF_hub()
+            
         parser = DataParser()
         return parser.get_dataset(tfrecord_path=CONFIG['data']['train_serialized_path'])
 
 
     def _get_model(self) -> tf.keras.Model:
-        """
-        Builds and compiles the DeepSign model.
-        
-        Returns:
-            tf.keras.Model: The compiled DeepSign model.
-        """
         img_height = CONFIG['data']['img_height']
         img_width = CONFIG['data']['img_width']
         embedding_dim = CONFIG['model']['embedding_dim']
 
+        deep_sign = DeepSign()
+        # Build and compile the model
+        model = deep_sign.build_and_compile_model(
+                                                    input_shape=(img_height, img_width, 1),
+                                                    embedding_dim=embedding_dim
+                                                )
+        return model
+
+
+    def _download_data_from_HF_hub(self):
+        """
+        Downloads the training data from Hugging Face Hub.
+
+        Returns:
+            downloaded_data: The downloaded data.
+        """
+
+        downloaded_data = None
         try:
-            deep_sign = DeepSign()
-            model = deep_sign.build_model(
-                                            input_shape=(img_height, img_width, 1),
-                                            embedding_dim=embedding_dim
-                                        )
+            downloaded_daa = hf_hub_download(
+                                                repo_id=CONFIG['data']['HuggingFace']['train_repo_id'], 
+                                                filename=CONFIG['data']['HuggingFace']['train_filename'] 
+                                            )
         except Exception as e:
-            logging.error(f"Error building model: {e}")
+            logging.error(f"Error downloading data from Hugging Face Hub: {e}")
             raise
         else:
-            return model
+            logging.info(f"Data downloaded successfully from Hugging Face Hub.")
+            return downloaded_data
 
 
-    def train(self, model: tf.keras.Model, train_dataset: tf.data.Dataset, val_dataset: tf.data.Dataset | None) -> None:
-        pass
+    def train(self) -> None:
+        try:
+            logging.info("Starting training...")
+            self.model.fit(
+                            self.dataset,
+                            epochs=CONFIG['training']['epochs'],
+                            callbacks=[
+                                tf.keras.callbacks.EarlyStopping(
+                                    monitor='val_loss',
+                                    patience=5,
+                                    restore_best_weights=True
+                                )
+                            ]
+                          )
+        except Exception as e:
+            logging.error(f"Error during training: {e}")
+            raise
+        else:
+            logging.info("Training completed successfully.")
+        
 
 
 
 if __name__ == "__main__":
     trainer = DeepSignTrainer()
-    # Assuming you have a model to train
-    # model = trainer._get_model()
-    # trainer.train(model, trainer.dataset, None)
+    # Start training
+    trainer.train()
 
